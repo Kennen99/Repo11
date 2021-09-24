0c1cb9793fdf3a895b2f4b546efe39bd4cf81937
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-24 08:27:12 +0200

    Added a js file to ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,31 @@
+// Only change code below this line
+function myMutation(arr){
+    var duzina = arr[0].length;
+    if(arr[1].length > duzina){
+        duzina = arr[1].length;
+    }
+    else{
+        // mjenjamo red od manjeg do veceg zbog petlje
+        var pom = arr[0];
+        arr[0] = arr[1];
+        arr[1] = pom;
+    }
+    arr[0] = arr[0].toLowerCase();
+    arr[1] = arr[1].toLowerCase();
+    for(var i=0; i<arr[0].length; i++){
+        var mozda = false;
+        for(var j=0; j<arr[1].length; j++){
+            if(arr[0][i] == arr[1][j]){
+                mozda = true;
+                break;
+            }
+        }
+        if(mozda === false){ 
+            return false;
+        }
+    }
+    return true;
+}
+// Only change code above this line
+
+console.log(myMutation(["Noel", "Ole"]));
