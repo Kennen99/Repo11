840cabcb6e6b9aec196ef448ef6a8ab355495d7d
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-27 08:51:47 +0200

    Added new changes to ex05

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -2,9 +2,11 @@
 var myNestedArray = [["Jaba", 1]];
 function myNestedFunction(arr){
     myNestedArray.pop();
-    myNestedArray.push(["Toblerone", 5], ["Milka", 3]);
+    for(var i=0; i<arr.length; i++){
+        myNestedArray.push(arr[i]);
+    }
     return myNestedArray;
 }
 // Only change code above this line
-console.log(myNestedFunction());
+console.log(myNestedFunction([["Toblerone", 5],["Milka", 3]]));
 module.exports = myNestedFunction;
